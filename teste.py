5988aaf946cf050992c0327b3b67f95edafc097b
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-07-26 20:39:42 -0300

    comentando coisas

--- teste.py
+++ teste.py
@@ -26,6 +26,10 @@ while loop==1:
     Rmin=0
     for i in range (1, Nx - 2):
         for j in range(1, Ny-2):
+            #Erro na linha abaixo: da um numero mto maior do que deveria
+            #Fiz baseada nessa linha do prog em MatLab
+            #Residue=w.*(0.25.*(V(i-1,j)+V(i+1,j)+V(i,j-1)+V(i,j+1)+rho(i,j))-V(i,j));
+            #A.*B is the element-by-element product of A and B
             Residue = w*(0.25*(V[i-1,j]+V[i+1,j]+V[i,j-1]+V[i,j+1]+Rho[i,j])-V[i,j]);
             Rmin = Rmin + abs(Residue)
             V[i,j]= V[i,j] + Residue
