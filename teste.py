cd763f2fc1b7562f171a693f4f2f366e4710d12d
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-07-26 21:36:48 -0300

    consertando
    ou estragando, sei lá

--- teste.py
+++ teste.py
@@ -22,18 +22,13 @@ while loop==1:
     Rmin=0
     for i in range (1, Nx - 2):
         for j in range(1, Ny-2):
-            #Erro na linha abaixo: da um numero mto maior do que deveria
-            #Fiz baseada nessa linha do prog em MatLab
-            #Residue=w.*(0.25.*(V(i-1,j)+V(i+1,j)+V(i,j-1)+V(i,j+1)+rho(i,j))-V(i,j));
-            #A.*B is the element-by-element product of A and B
-            Residue = w*(0.25*(V[i-1,j]+V[i+1,j]+V[i,j-1]+V[i,j+1]+Rho[i,j])-V[i,j]);
+            Residue = 0.25*(V[i-1,j]+V[i+1,j]+V[i,j-1]+V[i,j+1]+Rho[i,j])-V[i,j];
             Rmin = Rmin + abs(Residue)
             V[i,j]= V[i,j] + Residue
             
     Rmin=Rmin/(Nx*Ny) # Average Residue per grid point
-
-    if(Rmin>=0.00001):
-        Ncount=Ncount+1
+    Ncount = Ncount + 1
+    if(Rmin<=0.00001):
         if(Ncount>M):
             loop=0
             print "nao converge."
