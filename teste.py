23fce6c3e768cd25d27b8f3d974f2876c389583d
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-07-27 10:25:24 -0300

    corrigindo e fazendo funfar

--- teste.py
+++ teste.py
@@ -4,31 +4,41 @@ import math
 
 loop=1
 Nx=50
-Ny=50
+Ny=300
 M=4000
 
 #Matriz potencial
-V = np.zeros((Nx, Ny))
+V = np.zeros((Nx, Ny)) #y = x, x = y
+V[9, :] = 10 #tensao na placa superior
+V[39, :] = 5 #tensao na placa inferior
 
-#Matriz de cargas
-Rho = np.zeros((Nx, Ny))
-Rho[9, :] = 50 #carga na placa superior
-Rho[29, :] = -50 #carga na placa inferior
+#Condicoes de fronteira
+V[0,:]= 0
+V[Nx-1,:]=0
+V[:,0]=0
+V[:,Ny-1]=0
 
 w = math.cos(math.pi/Nx)+math.cos(math.pi/Ny) # Converging Term
-
 Ncount = 0
-while loop==1:
-    Rmin=0
-    for i in range (1, Nx - 2):
+while loop == 1:
+    Rmin = 0
+    for i in range (10, 38):
         for j in range(1, Ny-2):
-            Residue = 0.25*(V[i-1,j]+V[i+1,j]+V[i,j-1]+V[i,j+1]+Rho[i,j])-V[i,j];
+            Residue = 0.25*(V[i-1,j]+V[i+1,j]+V[i,j-1]+V[i,j+1])-V[i,j];
             Rmin = Rmin + abs(Residue)
             V[i,j]= V[i,j] + Residue
-            
     Rmin=Rmin/(Nx*Ny) # Average Residue per grid point
+
+    for i in range (38, 10, -1):
+        for j in range(1, Ny-2):
+            Residue = 0.25*(V[i-1,j]+V[i+1,j]+V[i,j-1]+V[i,j+1])-V[i,j];
+            Rmin = Rmin + abs(Residue)
+            V[i,j]= V[i,j] + Residue
+    Rmin=Rmin/(Nx*Ny) # Average Residue per grid point
+    
     Ncount = Ncount + 1
-    if(Rmin<=0.00001):
+    
+    if(Rmin<=0.0000000001):
         if(Ncount>M):
             loop=0
             print "nao converge."
