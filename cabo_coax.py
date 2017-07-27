25957b02b9fde9e8c10c9c92b5d7309081d2ff97
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-07-27 15:17:42 -0300

    concluindo o 2

--- cabo_coax.py
+++ cabo_coax.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 
 loop=1
 Nx=50
@@ -23,45 +22,29 @@ a, b = 25, 25
 width2, height2 = 11, 11
 r2 = 5
 
-EPSILON = 2.2
-
-# draw the circle
-for y in range(height1):
-    for x in range(width1):
-        # see if we're close to (x-a)**2 + (y-b)**2 == r**2
-        if (x-a)**2 + (y-b)**2 - r1**2 == 0:
-            print "Entro aqui."
-            V[y, x] = 0
+EPSILONp = 2.2
+EPSILONg = 3
 
 for y in range(height2, height1):
     for x in range(width2, width1):
-        # see if we're close to (x-a)**2 + (y-b)**2 == r**2
-        if abs((x - a)**2 + (y - b)**2 - r2**2) < EPSILON**2:
-            print "Entro aqui."
+        if abs((x - a)**2 + (y - b)**2 - r2**2) < EPSILONp**2:
             V[y, x] = 5
-
-plt.title("Potencial")
-plt.imshow(V, cmap='hot', interpolation='nearest')
-plt.show()
-
-
-w = math.cos(math.pi/Nx)+math.cos(math.pi/Ny) # Converging Term
 Ncount = 0
 while loop == 1:
     Rmin = 0
-    for i in range(height2, height1):
-        for j in range(width2, width1):
+    for i in range(Ny):
+        for j in range(Nx):
             # see if we're close to (x-a)**2 + (y-b)**2 == r**2
-            if abs((x - a) ** 2 + (y - b) ** 2 - r1 ** 2) < EPSILON ** 2:
+            if (abs((i-a)**2 + (j-b)**2) <= (r1**2 - EPSILONg**2) and abs((i-a)**2 + (j-b)**2) >= (r2**2 - EPSILONp**2)):
                 Residue = 0.25 * (V[i - 1, j] + V[i + 1, j] + V[i, j - 1] + V[i, j + 1]) - V[i, j];
                 Rmin = Rmin + abs(Residue)
                 V[i, j] = V[i, j] + Residue
     Rmin=Rmin/(Nx*Ny) # Average Residue per grid point
 
-    for i in range(height1, height2, -1):
-        for j in range(width1, width2, -1):
+    for i in range(Ny, 0, -1):
+        for j in range(Nx, 0, -1):
             # see if we're close to (x-a)**2 + (y-b)**2 == r**2
-            if abs((x - a) ** 2 + (y - b) ** 2 - r1 ** 2) < EPSILON ** 2:
+            if (abs((i - a) ** 2 + (j - b) ** 2) <= (r1 ** 2 - EPSILONg ** 2) and abs((i - a) ** 2 + (j - b) ** 2) >= (r2 ** 2 - EPSILONp ** 2)):
                 Residue = 0.25 * (V[i - 1, j] + V[i + 1, j] + V[i, j - 1] + V[i, j + 1]) - V[i, j];
                 Rmin = Rmin + abs(Residue)
                 V[i, j] = V[i, j] + Residue
@@ -79,7 +62,7 @@ plt.title("Potencial")
 plt.imshow(V, cmap='hot', interpolation='nearest')
 plt.show()
 
-y, x = np.mgrid[50:-50:100j, 500:-500:100j]
+y, x = np.mgrid[25:-25:100j, 25:-25:100j]
 dy, dx = np.gradient(V)
 fig, ax = plt.subplots()
 ax.quiver(x, y, dx, dy, V)
